0af99dece6b7f1939e658a69ed5354a056cfd8f9
Akihiro MOTOKI <motoki@da.jp.nec.com>
2012-06-25 16:07:33 +0900

    Pass configs in localrc to the ovs-install script.
    This commit enhances the quick installer. It is a preparation for
    multi node support with the installer. Now scripts/install-ovs-as-ofs.sh
    reads some configuration such as OFC_HOST and OVS_INTERFACE from localrc
    used by devstack. OFC_HOST specifies a OpenFlow controler IP address.
    If OVS_INTERFACE is defined as a non-empty value, an interface specified
    by this variable is attached to OVS_BRIDGE (br-int) in the script.

--- quickstart/installer.sh
+++ quickstart/installer.sh
@@ -8,13 +8,14 @@ DEVSTACK_REPO=http://github.com/openstack-dev/devstack.git
 DEVSTACK_DIR=devstack
 DEVSTACK_BRANCH=6bedba790250b9b67776645c690d29d58d94ceac
 
+LOCALRC=$CDIR/localrc
 
 $CDIR/scripts/config-cgroup-device-acl.sh
 
 SLICEABLE_PATCH="$CDIR/patches/trema/0001-fixed-create_filter-in-config.cgi.patch" \
     $CDIR/scripts/install-trema-sliceable-switch.sh
 
-$CDIR/scripts/install-ovs-as-ofs.sh
+LOCALRC=$LOCALRC $CDIR/scripts/install-ovs-as-ofs.sh
 
 [ -f /usr/bin/git ] || sudo apt-get -y install git
 git clone $DEVSTACK_REPO $DEVSTACK_DIR
@@ -22,6 +23,6 @@ pushd $DEVSTACK_DIR
 git checkout $DEVSTACK_BRANCH
 git am $CDIR/patches/devstack/0001-support-Quantum-NEC-OpenFlow-Plugin.patch
 git am $CDIR/patches/devstack/0002-support-http_proxy.patch
-cp $CDIR/localrc .
+cp $LOCALRC .
 ./stack.sh
 popd
--- quickstart/scripts/install-ovs-as-ofs.sh
+++ quickstart/scripts/install-ovs-as-ofs.sh
@@ -3,9 +3,13 @@
 # All Rights Reserved.
 #
 
+if [ -n "$LOCALRC" -a -e "$LOCALRC" ]; then
+    . $LOCALRC
+fi
+
 OFC_HOST=${OFC_HOST:-127.0.0.1}
 OVS_BRIDGE=${OVS_BRIDGE:-br-int}
-
+OVS_INTERFACE=${OVS_INTERFACE:-}
 
 kernel_version=`cat /proc/version | cut -d " " -f3`
 sudo apt-get install -y openvswitch-switch openvswitch-datapath-dkms linux-headers-$kernel_version
@@ -14,3 +18,6 @@ sudo ovs-vsctl --no-wait -- --if-exists del-br $OVS_BRIDGE
 sudo ovs-vsctl --no-wait add-br $OVS_BRIDGE
 sudo ovs-vsctl --no-wait set-controller $OVS_BRIDGE tcp:$OFC_HOST
 sudo /sbin/ifconfig $OVS_BRIDGE up
+if [ -n "$OVS_INTERFACE" ]; then
+    sudo ovs-vsctl add-port $OVS_BRIDGE $OVS_INTERFACE
+fi
